5e29dbca3b9aedbba1fedf261eed65a10ed67dc3
Alexandra Swan <alex@alex-macbook.local>
2021-01-10 06:29:01 +1100

    add more images

---
+++ test.js
@@ -0,0 +1,32 @@
+const testFiles = {
+  mp4: {
+    href:
+      " https://444vno15v5re20btub322y5h-wpengine.netdna-ssl.com/wp-content/uploads/2020/12/Kieran-Slate-@ksl_art-2.mp4",
+    title: " Kieran Slate || @ksl_art",
+  },
+  gif: {
+    href:
+      " https://444vno15v5re20btub322y5h-wpengine.netdna-ssl.com/wp-content/uploads/2020/12/@chevitarry.gif",
+    title: " @chevitarry",
+  },
+  png: {
+    href:
+      " https://444vno15v5re20btub322y5h-wpengine.netdna-ssl.com/wp-content/uploads/2020/11/@yettinim-yettinim.tumblr.png",
+    title: " @yettinim || yettinim.tumblr.com",
+  },
+  jpeg: {
+    href:
+      " https://444vno15v5re20btub322y5h-wpengine.netdna-ssl.com/wp-content/uploads/2020/11/Maria-Kakashkina-@Opal_Delight-scaled.jpeg",
+    title: " Maria Kakashkina || @Opal_Delight",
+  },
+  youtube: {
+    href: " https://www.youtube.com/embed/-wr-ZQ3Ppcw",
+    title: " Oliver Elm || @flyboy_elm",
+  },
+  jpg: {
+    href:
+      " https://444vno15v5re20btub322y5h-wpengine.netdna-ssl.com/wp-content/uploads/2020/11/Katie-Puppos-@Keppuos-B.jpg",
+    title: " Katie Puppos || @Keppuos",
+  },
+};
+const types = ["mp4", "gif", "png", "jpeg", "youtube", "jpg"];
